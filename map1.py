5605a616974d4e99b6912c3eecaaf950c95cfb70
Michael Ndon <manj19.mjn@gmail.com>
2020-09-21 23:32:07 +0100

    World Map

---
+++ map1.py
@@ -0,0 +1,43 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %sm
+"""
+
+def color_producer(elev):
+    if elev < 2000:
+        return 'green'
+    elif 2000 <= elev <= 3000:
+        return 'orange'
+    elif elev >= 3000:
+        return 'red'
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), radius=6, tooltip=folium.Tooltip("Click To See Info", sticky=False), 
+    fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Map1.html")
